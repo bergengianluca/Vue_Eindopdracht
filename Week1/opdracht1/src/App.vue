095a636c91436852eab68af2f6522478a68cc9ab
bergengianluca <gianluca.bergen@student.graafschapcollege.nl>
2026-04-20 11:43:36 +0200

    Add initial project structure with Vue and Vite setup
    - Created main application file (src/main.js) and entry point (src/App.vue).
    - Added HelloWorld component (src/components/HelloWorld.vue) with basic layout and functionality.
    - Implemented ticket reservation logic in App.vue with input fields for name and film.
    - Included SVG icons for social links and documentation in icons.svg.
    - Added assets: hero image (src/assets/hero.png), Vite logo (src/assets/vite.svg), and Vue logo (src/assets/vue.svg).
    - Configured Vite with vite.config.js for Vue support.
    - Initialized package-lock.json for dependency management.
    - Created style.css for styling the application.

---
+++ Week1/opdracht1/src/App.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { ref } from 'vue'
+
+const naam = ref('')
+const film = ref('')
+const prijs = ref(0);
+const teller = ref(1)
+const isReserveerd = ref(false) 
+
+function omhoog() {
+  if (teller.value <= 5)
+      teller.value++
+}
+
+function omlaag() {
+  if (teller.value >= 2)
+      teller.value--
+}
+
+function reserveer() {
+  prijs.value = teller.value * 12.50
+  isReserveerd.value = true
+}
+
+function reset() {
+  isReserveerd.value = false
+  naam.value = ""
+  film.value = ""
+  prijs.value = 0
+  teller.value = 1
+}
+
+
+</script>
+
+<template>
+  <input type="text" v-model="naam">
+  <input type="text" v-model="film"> <br>
+
+  <button @click="omhoog()">+</button>
+  <h1>{{ teller }}</h1>
+  <button @click="omlaag()">-</button>
+
+  <p>Prijs per ticket: EUR:12.50</p>
+
+  <button @click="reserveer">reserveer</button>
+  <button @click="reset">reset</button>
+  <h1 v-show="isReserveerd">{{ naam }} {{ film }} €{{ prijs }}</h1>
+  <h1 v-show="!isReserveerd">vul eerst je naam en film in</h1>
+</template>
+
+<style>
+
+</style>
