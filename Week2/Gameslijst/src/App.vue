6dd1faecbe940c914168e662e538018b60f96cf5
bergengianluca <gianluca.bergen@student.graafschapcollege.nl>
2026-05-11 11:22:52 +0200

    feat: initialize Vue project with Vite, add game list and icons
    - Created main application structure with App.vue and main.js
    - Added HelloWorld component with Vite and Vue logos
    - Implemented game list in App.vue
    - Included SVG icons for social media and documentation
    - Added styles for layout and responsiveness
    - Imported assets including images and SVGs
    - Configured Vite for Vue support

---
+++ Week2/Gameslijst/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue'
+
+const ingelogd = ref(true);
+
+const games = [ "GTA", "Battlefield", "FIFA", "Minecraft"];
+</script>
+
+<template>
+  <template v-for="game in games" :key="game">
+    <li>{{ game }}</li>
+  </template>
+</template>
