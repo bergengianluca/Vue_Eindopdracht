d2fa42bf98b815c007984bc28c755f207bf5044b
bergengianluca <gianluca.bergen@student.graafschapcollege.nl>
2026-06-23 10:42:20 +0200

    dingen dat ik niet begrijp heb ik verwijderd

--- EindOpdracht/pokemonapp/src/App.vue
+++ EindOpdracht/pokemonapp/src/App.vue
@@ -1,10 +1,5 @@
-<script setup>
-import InstallPrompt from '@/components/InstallPrompt.vue'
-</script>
-
 <template>
   <RouterView />
-  <InstallPrompt />
 </template>
 
 <style scoped></style>
--- EindOpdracht/pokemonapp/src/main.js
+++ EindOpdracht/pokemonapp/src/main.js
@@ -7,10 +7,3 @@ const app = createApp(App)
 app.use(router)
 
 app.mount('#app')
-
-// Een service worker zorgt ervoor dat de browser de website als PWA kan installeren.
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register(`${import.meta.env.BASE_URL}service-worker.js`)
-  })
-}
